cf503a4883485dc40415d1a688b2090fd21a8b9a
1gamach <112381438+1hohmach@users.noreply.github.com>
2022-10-07 19:24:36 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3,77 +3,77 @@ require('dotenv').config();
 const tmi = require('tmi.js');
 
 const client = new tmi.Client({
-    connection: {
-        reconnect: true
-    },
-	channels: [ '1gamach' ],
-    identity: {
-		username: process.env.TWITCH_BOT_USERNAME,
-		password: process.env.TWITCH_OAUTH_TOKEN
-	}
+  connection: {
+    reconnect: true
+  },
+  channels: ['ravshann'],
+  identity: {
+    username: process.env.TWITCH_BOT_USERNAME,
+    password: process.env.TWITCH_OAUTH_TOKEN
+  }
 });
 
 client.connect();
 
 client.on('message', (channel, userstate, message, self) => {
-    if(self) return;
-    let messageRepeatCount = message.split(' ')
+  if (self) return;
+  let messageRepeatCount = message.split(' ')
 
-    if (message.toLowerCase() === '%ping' && userstate.username === '1gamach') {
-      client.say(channel, `pong`) 
-    }
+  if (message.toLowerCase() === '%ping' && userstate.username === '1gamach') {
+    client.say(channel, `pong`)
+  }
 
-    if (message.startsWith('!spam') && userstate.username === '1gamach') {
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `${message.slice(8)}`);
-      } 
+  if ((message.startsWith('!спам') || message.startsWith('!spam')) && userstate.username === '1gamach') {
+    for (let i = 0; i < + messageRepeatCount[1]; i++) {
+      client.say(channel, `${message.slice(8)}`);
     }
-    // тг
-    if (message.toLowerCase() === '!тг' || message.toLowerCase() === '!tg') {
-      client.say(channel, `@${userstate.username} TG - https://t.me/RavshanNstream MorphinTime`)
+  }
+  // тг
+  if (message.toLowerCase() === '!тг' || message.toLowerCase() === '!tg') {
+    client.say(channel, `@${userstate.username} TG - https://t.me/RavshanNstream MorphinTime`)
+  }
+  if ((message.startsWith('!tg') || message.startsWith('!тг')) && userstate.username === 'ravshann') {
+    for (let i = 0; i < + messageRepeatCount[1]; i++) {
+      client.say(channel, `TG - https://t.me/RavshanNstream MorphinTime`);
     }
-    if ((message.startsWith('!tg') || message.startsWith('!тг')) && userstate.username === 'ravshann') { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `TG - https://t.me/RavshanNstream MorphinTime`);
-      }
+  }
+  // инст
+  if (message.toLowerCase() === '!инст' || message.toLowerCase() === '!inst') {
+    client.say(channel, `@${userstate.username} inst - https://www.instagram.com/ravshann_13/ MorphinTime`)
+  }
+  if ((message.startsWith('!инст') || message.startsWith('!inst')) && userstate.username === 'ravshann') {
+    for (let i = 0; i < + messageRepeatCount[1]; i++) {
+      client.say(channel, `inst -  https://www.instagram.com/ravshann_13/ MorphinTime`);
     }
-    // инст
-    if (message.toLowerCase() === '!инст' || message.toLowerCase() === '!inst') {
-      client.say(channel, `@${userstate.username} inst - https://www.instagram.com/ravshann_13/ MorphinTime`)
+  }
+  // winline
+  if (message.startsWith('!wl') && userstate.username === 'ravshann') {
+    for (let i = 0; i < + messageRepeatCount[1]; i++) {
+      client.say(channel, `WINLINE промо: rav - https://winline.tv/3pWr1GS winline-1`);
     }
-    if ((message.startsWith('!инст') || message.startsWith('!inst')) && userstate.username === 'ravshann') { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `inst -  https://www.instagram.com/ravshann_13/ MorphinTime`);
-      }
+  }
+  // гта
+  if ((message.startsWith('!gta') || message.startsWith('!гта')) && userstate.username === '1gamach') {
+    for (let i = 0; i < + messageRepeatCount[1]; i++) {
+      client.say(channel, `Arizona RP | Promo: STAYA Подробнее: https://arizona-v.com/`);
     }
-    // каз
-    if (message.toLowerCase() === '!каз') {
-      client.say(channel, `@${userstate.username} 😺 https://t.me/RavshanNstream/5019`) 
+  }
+  // дискорд
+  if (message.toLowerCase() === '!дс' || message.toLowerCase() === '!ds') {
+    client.say(channel, `@${userstate.username} https://discord.gg/RhWqUd2P`)
+  }
+  // донат
+  if (message.toLowerCase() === '!донат' || message.toLowerCase() === '!donat') {
+    client.say(channel, `@${userstate.username} https://www.donationalerts.com/r/ravshann`)
+  }
+  // getx
+  if (message.startsWith('!гёт') && userstate.username === 'ravshann') {
+    for (let i = 0; i < + messageRepeatCount[1]; i++) {
+      client.say(channel, `inst -  https://www.instagram.com/ravshann_13/ MorphinTime`);
     }
-    if (message.startsWith('!каз') && userstate.username === 'ravshann') { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `😺 https://t.me/RavshanNstream/5019`);
-      }
-    }
-    // winline
-    if (message.startsWith('!wl') && userstate.username === 'ravshann') { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `WINLINE промо: rav - https://winline.tv/3pWr1GS winline-1`);
-      }
-    }
-    // гта
-    if ((message.startsWith('!gta') || message.startsWith('!гта')) && userstate.username === '1gamach') {
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `Arizona RP | Promo: STAYA Подробнее: https://arizona-v.com/`);
-      }
-    }
-    // дискорд
-    if (message.toLowerCase() === '!дс' || message.toLowerCase() === '!ds') {
-      client.say(channel, `@${userstate.username} https://discord.gg/RhWqUd2P`) 
-    }
-
-  onMessageHandler (channel, userstate, message)
-  onMessageHandler2 (channel, userstate, message)
+  }
+  onMessageHandler(channel, userstate, message)
+  onMessageHandler2(channel, userstate, message)
 });
 
 // SPAM MODERATORS
@@ -82,27 +82,27 @@ client.on('chat', (channel, user, message, userstate) => {
   if (user.mod) {
     let messageRepeatCount = message.split(' ')
 
-    if (message.toLowerCase() === '!songlist') {
+    if (message.toLowerCase() === '%songlist') {
       client.say(channel, `@${userstate.username} https://streamelements.com/ravshann/mediarequest`)
     }
-    if (message.startsWith('!tg') || message.startsWith('!тг')) { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
+    if (message.startsWith('!tg') || message.startsWith('!тг')) {
+      for (let i = 0; i < + messageRepeatCount[1]; i++) {
         client.say(channel, `TG - https://t.me/RavshanNstream MorphinTime`);
       }
     }
-    if (message.startsWith('!wl')) { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
+    if (message.startsWith('!wl')) {
+      for (let i = 0; i < + messageRepeatCount[1]; i++) {
         client.say(channel, `WINLINE промо: rav - https://winline.tv/3pWr1GS winline-1`);
       }
     }
-    if (message.startsWith('!каз')) { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `😺 https://t.me/RavshanNstream/5019 MorphinTime`);
+    if (message.startsWith('!инст') || message.startsWith('!inst')) {
+      for (let i = 0; i < + messageRepeatCount[1]; i++) {
+        client.say(channel, `inst -  https://www.instagram.com/ravshann_13/ MorphinTime`);
       }
     }
-    if (message.startsWith('!инст') || message.startsWith('!inst')) { 
-      for (let i = 0; i <+ messageRepeatCount[1]; i++) {
-        client.say(channel, `inst -  https://www.instagram.com/ravshann_13/ MorphinTime`);
+    if (message.startsWith('!гёт') || message.startsWith('!get')) {
+      for (let i = 0; i < + messageRepeatCount[1]; i++) {
+        client.say(channel, `Играй со мной на GetX, с промокодом: VOZDUH - https://t.me/RavshanNstream/5713`);
       }
     }
   }
@@ -111,14 +111,43 @@ client.on('chat', (channel, user, message, userstate) => {
 // EMOTEONLY on/off
 
 client.on('message', (channel, userstate, message, self) => {
-  if (message.toLowerCase() === '!on' && userstate.username === '1gamach') { 
+  if (message.toLowerCase() === '!on' && userstate.username === '1gamach') {
     client.emoteonly(channel)
   }
-  if (message.toLowerCase() === '!off' && userstate.username === '1gamach') { 
-    client.emoteonlyoff(channel)  
+  if (message.toLowerCase() === '!off' && userstate.username === '1gamach') {
+    client.emoteonlyoff(channel)
   }
 })
 
+// DELETE MESSAGE
+
+function onMessageHandler(channel, userstate, message) {
+   checkTwitchChat(userstate, message, channel)
+}
+
+function onMessageHandler2(channel, userstate, message) {
+//    checkTwitchChat2(userstate, message, channel)
+}
+
+function checkTwitchChat(userstate, message, channel) {
+  let BLOCKED_WORDS1 = ['z', 'v', 'zv', 'vz', 'zz', 'vv', 'zzz', 'vvv', 'zvz', 'vzv', 'z v', 'v z', 'zzzz', 'vvvv', 'zzvv', 'vvzz']
+  message = message.toLowerCase()
+  for (var i = 0; i < BLOCKED_WORDS1.length; i++) {
+    if (BLOCKED_WORDS1[i] === message) {
+      client.deletemessage(channel, userstate.id)
+    }
+  }
+}
+
+function checkTwitchChat2(userstate, message, channel) {
+  message = message.toLowerCase()
+  let shouldSendMessage = false
+  BLOCKED_WORDS = ['z', 'v']
+  shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
+  if (shouldSendMessage) {
+    client.deletemessage(channel, userstate.id)
+  }
+}
 
 // TIMEOUT USER
 
@@ -133,33 +162,3 @@ client.on('message', (channel, userstate, message, self) => {
 //    }
 //  }
 //})
-
-// DELETE MESSAGE
-
-function onMessageHandler (channel, userstate, message) {
-  checkTwitchChat(userstate, message, channel)
-} 
-
-function onMessageHandler2 (channel, userstate, message) {
-//  checkTwitchChat2(userstate, message, channel)
-}
-
-function checkTwitchChat(userstate, message, channel) {
-  let BLOCKED_WORDS1 = ['z', 'v', 'zv', 'vz', 'zz', 'vv', 'zzz', 'vvv', 'zvz', 'vzv', 'z v']
-  message = message.toLowerCase()
-  for (var i = 0; i < BLOCKED_WORDS1.length; i++) {
-        if (BLOCKED_WORDS1[i] === message) {
-            client.deletemessage(channel, userstate.id)
-        }
-    }
-} 
-
-function checkTwitchChat2(userstate, message, channel) {
-  message = message.toLowerCase()
-  let shouldSendMessage = false
-  BLOCKED_WORDS = ['z', 'v']
-  shouldSendMessage = BLOCKED_WORDS.some(blockedWord => message.includes(blockedWord.toLowerCase()))
-  if (shouldSendMessage) {
-    client.deletemessage(channel, userstate.id)
-  }
-}
