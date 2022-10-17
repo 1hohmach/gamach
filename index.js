aeb17e28d28640823b1c5e4f761e8f14ff505359
1hohmach <112381438+1hohmach@users.noreply.github.com>
2022-10-17 21:54:37 +0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const client = new tmi.Client({
   connection: {
     reconnect: true
   },
-  channels: ['ravshann'],
+  channels: ['1gamach'],
   identity: {
     username: process.env.TWITCH_BOT_USERNAME,
     password: process.env.TWITCH_OAUTH_TOKEN
